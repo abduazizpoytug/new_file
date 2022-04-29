6da73ca5929ec25b34ecccfb0440bb55ef90a2fc
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:13:32 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,39 @@
+a = [
+    "python", "html", "bootstrap" ,"css" , "c++" , "java" , "javaScript" ,"php", "paskal"
+]
+value = input("dastulash tili: ")
+
+
+if value == 'python':
+    print(f"siz allaqachon borsiz" )
+if value == 'html':
+    print(f"siz allaqachon borsiz" )
+if value == 'bootstrap':
+    print(f"siz allaqachon borsiz" )
+if value == 'css':
+    print(f"siz allaqachon borsiz" )
+if value == 'c++':
+    print(f"siz allaqachon borsiz" )
+if value == 'java':
+    print(f"siz allaqachon borsiz" )
+if value == 'javaScript':
+    print(f"siz allaqachon borsiz" )
+if value == 'php':
+    print(f"siz allaqachon borsiz" )
+if value == 'paskal':
+    print(f"siz allaqachon borsiz" )
+elif value == '!':
+    print("siz hozircha yoqsiz" )
+
+
+
+
+
+# name = "jamshid"
+# last_name = "tursunuv"
+
+# # result = "salom {} {}".format(name, last_name)
+
+# result = f"Salom {name} {last_name} nima gap"
+
+# print(result)
